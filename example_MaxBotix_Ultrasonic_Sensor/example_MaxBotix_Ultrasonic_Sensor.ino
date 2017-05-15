3c73cb7ee34d645e18eae960cc0b8900b577494a
Fèlix Vinyals <felix@felixvinyals.com>
2017-05-15 10:28:09 +0200

    Example updated

--- example_MaxBotix_Ultrasonic_Sensor/example_MaxBotix_Ultrasonic_Sensor.ino
+++ example_MaxBotix_Ultrasonic_Sensor/example_MaxBotix_Ultrasonic_Sensor.ino
@@ -1,15 +1,34 @@
 #include "MaxBotix_Ultrasonic_Sensor.h"
 
-#define TxPin 48
-#define RxPin 49 // This pin is not implemented in the MaxBotix sensor but is needed to initialize the SoftwareSerial Library that is inside this MaxBotix library.
+#define RxPin 50  // This pin is not implemented in the MaxBotix 
+                  // sensor but is needed to initialize the 
+                  // SoftwareSerial Library that is inside this MaxBotix library.
+#define TxPin 51
 
-MaxBotix sensor_ultrasonic(TxPin, RxPin);
+MaxBotix ultrasonicSensor(RxPin, TxPin);
 
 void setup() {
   Serial.begin(9600);
-
+  Serial.println("Fi setup");
 }
 
 void loop() {
-  Serial.println(sensor_ultrasonic.getDistance());
+  int distance;
+
+  distance = ultrasonicSensor.getDistance();
+
+  if (distance == -1) {
+    Serial.println("Timeout");  
+  }
+  
+  else if (distance == -2) {
+    Serial.println("Invalid Data");  
+  }
+
+  else {
+    Serial.print("Distance: ");
+    Serial.println(distance);  
+  }
 }
+
+
